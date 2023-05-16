a3a02078b33e22ded08a79d81dfe691cfb84c1d5
Dawid Komęza <dawid.komeza@gmail.com>
2023-05-16 15:48:49 +0200

    :sparkles: Add function files

---
+++ code/src/display/display.hpp
@@ -0,0 +1,11 @@
+#ifndef DISPLAY_H
+#define DISPLAY_H
+
+#include <Arduino.h>
+
+class Display {
+};
+
+extern Display display;
+
+#endif
---
+++ code/src/io/io.hpp
@@ -0,0 +1,11 @@
+#ifndef IO_H
+#define IO_H
+
+#include <Arduino.h>
+
+class IO {
+};
+
+extern IO io;
+
+#endif 
