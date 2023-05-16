68675ddd0a1da5659d0ccf7d6c56b17117c4c15b
Dawid Komęza <dawid.komeza@gmail.com>
2023-05-16 16:04:02 +0200

    :sparkles: Add Controller class implementation

--- code/src/controller/controller.cpp
+++ code/src/controller/controller.cpp
@@ -1,1 +1,29 @@
 #include "controller.hpp"
+
+void Controller::setPower(bool power) {
+    if (power == _power) return;
+
+    _power = power;
+}
+
+void Controller::setGear(int gear) {
+    if (gear == _gear) return;
+
+    _gear = gear;
+}
+
+void Controller::setThrottle(int throttle) {
+    if (throttle == _throttle) return;
+
+    _throttle = throttle;
+}
+
+void Controller::setSoftStart(bool softStart) {
+    if (softStart == _softStart) return;
+
+    _softStart = softStart;
+}
+
+int Controller::getVoltage() {
+    return _voltage;
+}
--- code/src/controller/controller.hpp
+++ code/src/controller/controller.hpp
@@ -2,9 +2,21 @@
 #define CONTROLLER_H
 
 #include <Arduino.h>
+#include "../settings/settings.hpp"
 
 class Controller {
-
+    public:
+        void setPower(bool power);
+        void setGear(int gear);
+        void setThrottle(int throttle);
+        void setSoftStart(bool softStart);
+        int getVoltage();
+    private:
+        int _gear = 0;
+        int _throttle = 0;
+        bool _power = false;
+        bool _softStart = true;
+        int _voltage = 0;
 };
 
 extern Controller controller;
