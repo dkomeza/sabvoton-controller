7442328337f57145bdcc3c406e2a7192ed86d2e2
Dawid Komęza <dawid.komeza@gmail.com>
2023-06-23 16:02:21 +0200

    :sparkles: Send data to the display

--- software/src/display/display.cpp
+++ software/src/display/display.cpp
@@ -7,3 +7,22 @@ Display::Display()
 void Display::update()
 {
 }
+
+void Display::receiveData()
+{
+}
+
+void Display::sendData()
+{
+    byte buffer[SEND_PACKET_SIZE];
+
+    buffer[0] = data.speed;
+    buffer[1] = data.mode << 4 | data.brake << 3 | data.gear;
+    buffer[2] = data.engineTemp;
+    buffer[3] = data.controllerTemp;
+    buffer[4] = data.batteryTemp;
+    buffer[5] = data.batteryVoltage >> 8;
+    buffer[6] = data.batteryVoltage;
+
+    displaySerial.write(buffer, SEND_PACKET_SIZE);
+}
--- software/src/display/display.hpp
+++ software/src/display/display.hpp
@@ -2,12 +2,26 @@
 #define DISPLAY_H
 
 #include <Arduino.h>
+#include <HardwareSerial.h>
+
+#include "../data/data.hpp"
+
+#define SEND_PACKET_SIZE 7
 
 class Display
 {
 public:
-    Display();
+    Display()
+    {
+        displaySerial.begin(9600, SERIAL_8N1, 16, 17);
+    };
     void update();
+
+private:
+    HardwareSerial displaySerial = HardwareSerial(2);
+
+    void receiveData();
+    void sendData();
 };
 
 extern Display display;
